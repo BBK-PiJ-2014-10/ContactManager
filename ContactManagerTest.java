7800039c685b6166fb46a58db17d368533d94c9f
konradko <konrad.korzel@gmail.com>
2015-03-15 19:13:34 +0000

    Add tests for getPastMeetingList, getPastMeetingList and addNewPastMeeting

--- ContactManagerTest.java
+++ ContactManagerTest.java
@@ -6,20 +6,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 
 public class ContactManagerTest {
 
-    private ContactManager contactManager;
+    private ContactManager contactManager = new ContactManagerImpl();
     private Meeting testMeeting;
+    private Contact testContact = new ContactImpl("test name", "");
     private Calendar testDate = Calendar.getInstance();
     private Set<Contact> testContacts = new HashSet<Contact>();
     private String testNotes = "test notes";
 
     @Before
     public void setUp() {
-        contactManager = new ContactManagerImpl();
-        testContacts.add(new ContactImpl("test name", ""));
+        testContacts.add(testContact);
         testMeeting = new MeetingImpl();
         testMeeting.setContacts(testContacts);
         testMeeting.setDate(testDate);
@@ -40,10 +41,11 @@ public class ContactManagerTest {
     public void testGetPastMeeting() {
         Calendar pastDate = Calendar.getInstance();
         pastDate.add(Calendar.DAY_OF_YEAR, -1);
-        int id = contactManager.addNewPastMeeting();
-        PastMeeting pastMeeting = contactManager.getPastMeeting(id);
-        assertEquals(pastDate, pastMeeting.getDate());
-        assertEquals(testContacts, pastMeeting.getContacts());
+        contactManager.addNewPastMeeting(testContacts, pastDate, testNotes);
+        PastMeeting expectedPastMeeting = contactManager.getPastMeetingList(this.testContact).get(-1);
+        PastMeeting resultPastMeeting = contactManager.getPastMeeting(expectedPastMeeting.getId());
+
+        assertEquals(expectedPastMeeting, resultPastMeeting);
     }
 
     @Test
@@ -68,12 +70,25 @@ public class ContactManagerTest {
 
     @Test
     public void testGetPastMeetingList() {
+        ContactManager testContactManager = new ContactManagerImpl();
+        Calendar pastDate = Calendar.getInstance();
+        pastDate.add(Calendar.DAY_OF_YEAR, -1);
+        int pastMeetingsNumberBefore = testContactManager.getPastMeetingList(this.testContact).size();
+        testContactManager.addNewPastMeeting(testContacts, pastDate, testNotes);
+        int pastMeetingsNumberAfter = testContactManager.getPastMeetingList(this.testContact).size();
 
+        assertTrue((pastMeetingsNumberBefore + 1) == pastMeetingsNumberAfter);
     }
 
     @Test
     public void testAddNewPastMeeting() {
+        Calendar pastDate = Calendar.getInstance();
+        pastDate.add(Calendar.DAY_OF_YEAR, -1);
+        int pastMeetingsNumberBefore = contactManager.getPastMeetingList(this.testContact).size();
+        contactManager.addNewPastMeeting(testContacts, pastDate, testNotes);
+        int pastMeetingsNumberAfter = contactManager.getPastMeetingList(this.testContact).size();
 
+        assertTrue((pastMeetingsNumberBefore + 1) == pastMeetingsNumberAfter);
     }
 
     @Test
